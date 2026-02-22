7e395917a806e8ceb30a58ab18ed12b83f0d4b5a
yallseetheshoes <yallseetheshoes@users.noreply.github.com>
2026-02-22 21:59:12 +0100

    Add IPC bridge with connection state

--- windows/loader/preload.js
+++ windows/loader/preload.js
@@ -1,21 +1,7 @@
-/**
- * The preload script runs before `loader.html` is loaded
- * in the renderer. It has access to web APIs as well as
- * Electron's renderer process modules and some polyfilled
- * Node.js functions.
- *
- * https://www.electronjs.org/docs/latest/tutorial/sandbox
- */
-
 
 
 const { contextBridge, ipcRenderer } = require('electron/renderer')
 
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-
-})
+contextBridge.exposeInMainWorld('electron', {
+  connectionState: (callback) => ipcRenderer.on('server-connection', callback)
+});
