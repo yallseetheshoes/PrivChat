4f94862c5ae0d86ed85d97f694ac02e634a670c3
yallseetheshoes <yallseetheshoes@users.noreply.github.com>
2026-02-24 13:48:43 +0100

    Add listener for form submit to stop reload

--- windows/loader/renderer.js
+++ windows/loader/renderer.js
@@ -1,6 +1,6 @@
 const radarCircle = document.getElementById('loaderCircle')
 const loaderText = document.getElementsByClassName('loadText')[0]
-const pages = document.getElementsByClassName('pages')[0]
+const pages = document.querySelector('.pages')
 window.electron.connectionState((event, args) => {
     if (args === 'Connection successful') {
         loaderText.innerHTML = 'Connection successful!'
@@ -11,3 +11,8 @@ window.electron.connectionState((event, args) => {
         },1000)
     }
 });
+
+const loginPage = document.querySelector('.page.login');
+loginPage.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
